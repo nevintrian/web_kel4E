b2ea4f5a124c6007cf201ed94b4c65b7e1f6abe6
brianv4 <55471120+brianv4@users.noreply.github.com>
2020-04-11 10:11:38 +0700

    Update v_sidebar.php

--- inventori/application/views/v_sidebar.php
+++ inventori/application/views/v_sidebar.php
@@ -29,7 +29,7 @@
                     </li>
                 </li>
                
-                <li class="list-group-item"><a href="<?php echo base_url()?>login/logout"><i class="glyphicon glyphicon-share"></i>Logout </a></li>
+                <li class="list-group-item"><a href="<?php echo base_url()?>login/logout" type="submit" onclick="javasciprt: return confirm('Apa Anda Yakin?')" ><i class="glyphicon glyphicon-share"></i>Logout </a></li>
 
                 <?php 
                 } elseif ($this->session->userdata('level') == 'manajer') {
