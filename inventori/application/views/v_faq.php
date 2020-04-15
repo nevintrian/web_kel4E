f7f7a6d748ccef2256dce924e4079a7a6ab897a5
nevintrian <ne.vind99@gmail.com>
2020-04-15 08:59:04 +0700

    faq coba

--- inventori/application/views/v_faq.php
+++ inventori/application/views/v_faq.php
@@ -1,8 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-	<link rel="stylesheet" href="assets/dist/css/site.min.css">
-    <script type="text/javascript" src="assets/dist/js/site.min.js"></script>
+
+  <style type="text/css">
+	[aria-expanded="false"] > .expanded, [aria-expanded="true"] > .collapsed {
+		display: none;
+	}
+</style>
 	</head>
 
 
@@ -11,15 +15,55 @@
       <div class="panel panel-default">
         <div class="panel-body">
   <div align="center" > <h3>FAQ</h3> </div>
-  <ul class="contact-info-list">
-  <li><span>Bagaimana cara order barang?</a></li>
-  <li><span>Berapa lama waktu proses pengiriman barang?</a></li>
-  <li><span>Bagaimana cara daftar akun?</a></li>
-  <li><span>kontak person UD SRI REJEKI?</a></li>
-  <li><span>bagaimana cara login?</a></li>
-  <li><span>bagaimana cara logout?</a></li>
-  <li><span>bagaimana cara hapus akun?</a></li>
-  </ul>
+<form>
+  <div id="accordion">
+	<div class="card">
+		<div class="card-header">
+			<a class="card-link" data-toggle="collapse" href="#menuone" aria-expanded="false" aria-controls="menuone">
+				<span class="collapsed"><i class="fa fa-plus"></i></span> 
+				<span class="expanded"><i class="fa fa-minus"></i></span> 
+				Bagaimana cara nyolong di UD Sri Rejeki?
+			</a>
+		</div>
+		<div id="menuone" class="collapse">
+		  <div class="card-body">
+			Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+		  </div>
+		</div>
+	</div>
+  </br>
+    <div class="card">
+        <div class="card-header">
+		  	<a class="card-link" data-toggle="collapse" href="#menutwo" aria-expanded="false" aria-controls="menutwo">
+				<span class="collapsed"><i class="fa fa-plus"></i></span> 
+				<span class="expanded"><i class="fa fa-minus"></i></span> 
+				Bagaimana cara mendaftar?
+			</a>
+        </div>
+        <div id="menutwo" class="collapse">
+          <div class="card-body">
+			Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          </div>
+        </div>
+    </div>
+</br>
+    <div class="card">
+        <div class="card-header">
+			<a class="card-link" data-toggle="collapse"  href="#menu3" aria-expanded="false" aria-controls="menu3">
+				<span class="collapsed"><i class="fa fa-plus"></i></span> 
+				<span class="expanded"><i class="fa fa-minus"></i></span> 
+				Bagaimana cara membeli bdarang di UD Sri Rejeki?
+			</a>
+        </div>
+        <div id="menu3" class="collapse">
+          <div class="card-body">
+			Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+          </div>
+        </div>
+    </div>
+</div>
+</br>
+</form>
   </div> 
   </div> 
   </div> 
