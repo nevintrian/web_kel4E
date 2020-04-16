ccdecf318ab61e9c4ec713632a448119ef59f6c2
brianv4 <55471120+brianv4@users.noreply.github.com>
2020-04-16 20:54:46 +0700

    notif status

--- inventori/application/views/v_header.php
+++ inventori/application/views/v_header.php
@@ -44,16 +44,16 @@
                  ?>
           <div id="navbar" class="navbar-collapse collapse">
             <div class="navbar-form navbar-right">
+            <a href="<?php echo base_url() ?>notifikasi" type="submit" class="btn btn-info"><i class='fas fa-bell' style='font-size:16px'></i> </a>
             <a href="<?php echo base_url() ?>keranjang" type="submit" class="btn btn-info"><i class='fas fa-shopping-cart' style='font-size:16px'></i> </a>
-
-               <a href="<?php echo base_url() ?>dashboard" type="submit" class="btn btn-primary"><?php echo $this->session->userdata("username") ?> </a> <!-- session username -->
-               <a href="<?php echo base_url() ?>login/logout" type="submit" class="btn btn-danger" onclick="javasciprt: return confirm('Apa Anda Yakin?')" >Logout </a> <!-- session username -->
+            <a href="<?php echo base_url() ?>dashboard" type="submit" class="btn btn-primary"><?php echo $this->session->userdata("username") ?> </a> <!-- session username -->
+            <a href="<?php echo base_url() ?>login/logout" type="submit" class="btn btn-danger" onclick="javasciprt: return confirm('Apa Anda Yakin?')" >Logout </a> <!-- session username -->
             </div>
           </div>
               <?php }else{ ?>
                 <div id="navbar" class="navbar-collapse collapse">
             <div class="navbar-form navbar-right">
-            <a href="<?php echo base_url() ?>notifikasi" type="submit" class="btn btn-info"><i class='fas fa-bell' style='font-size:16px'></i> </a>
+            
             <a href="<?php echo base_url() ?>keranjang" type="submit" class="btn btn-info"><i class='fas fa-shopping-cart' style='font-size:16px'></i> </a>
             <a href="<?php echo base_url() ?>login" type="submit" class="btn btn-primary">login </a> <!-- session username -->
             <a href="<?php echo base_url() ?>daftar" type="submit" class="btn btn-primary">Register </a> <!-- session username -->
