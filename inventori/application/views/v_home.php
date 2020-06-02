b7906545a990faef369e8bef9012378f2d488df8
nevintrian <ne.vind99@gmail.com>
2020-06-02 13:29:10 +0700

    coba filter

--- inventori/application/views/v_home.php
+++ inventori/application/views/v_home.php
@@ -22,16 +22,35 @@
       <div class="panel panel-default">
         <div class="panel-body">
         <div class="card">
-        <div class="col-md-1 text-right">
-                <select id="id_barang" name="id_barang"  class="form-control" > 
-                <option value="">pilih</option>
-                <?php 
-                $sql = $this->db->query("select distinct jenis from barang");
-                foreach ($sql->result() as $row) {
-                 ?>
-                <option value="<?php echo $row->jenis ?>"><?php echo $row->jenis ?></option>
-                <?php } ?>
-                 </select>
+        <div class="col-md-3 text-right">
+        <div class="col-md-3 text-left">
+                <form action="<?php echo site_url('home/index'); ?>" class="form-inline" method="get">
+                    <div class="input-group">
+                       
+                        <select name="q" class="form-control" > 
+                                <option value="">pilih</option>
+                                <?php 
+                                $sql = $this->db->query("select distinct jenis from barang");
+                                foreach ($sql->result() as $row) {
+                                ?>
+                                <option value="<?php echo $row->jenis ?>"><?php echo $row->jenis ?></option>
+                                <?php } ?>
+                                </select>
+                        <span class="input-group-btn">
+                            <?php 
+                                if ($q <> '')
+                                {
+                                    ?>
+                                    <a href="<?php echo site_url('home'); ?>" class="btn btn-default">Reset</a>
+                                    <?php
+                                }
+                            ?>
+                          <button class="btn btn-primary" type="submit">Search</button>
+                        </span>
+                    </div>
+                </form>
+
+                </div>
                 
                 </div>
                 <div class="col-md-3 text-left">
